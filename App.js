b0cbc9ec386087f66d22682473cf875a4d7e91f8
Ganesh-9676 <ganeshneellapala938114@gmail.com>
2024-12-23 11:42:05 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-/*
-*
-*
-*<div id="parent">
-*   <div id="child">
-*       <h1>I'm an h1 tag</h1>
-*       <h2>I'm an h2 tag</h2>
-*   </div>
-*   <div id="child2">
-*       <h1>I'm an h1 tag</h1>
-*       <h2>I'm an h2 tag</h2>
-*   </div>
-*</div>
-*
-*
-*
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 const parent = React.createElement(
@@ -23,20 +8,20 @@ const parent = React.createElement(
     [React.createElement(
         "div",
         {id : "child"},
-        [React.createElement("h1",{},"I'm an h1 tag"), 
+        [React.createElement("h1",{},"This is Namaste React 🚀"), 
         React.createElement("h2",{},"I'm an h2 tag")]
     ),
     React.createElement(
         "div",
         {id : "child2"},
         [React.createElement("h1",{},"I'm an h1 tag"), 
-        React.createElement("h2",{},"I'm an h2 tag")]
-    )]
+        React.createElement("h2",{},"I'm an h2 tag"),]
+    ),]
 );
 
 
 
-    console.log(parent);
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
